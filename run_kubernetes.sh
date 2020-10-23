5b26172aea80c58c3d642c7ec595c972118fc6fe
Alex Norum <37006989+anorum@users.noreply.github.com>
2020-10-23 16:23:03 -0700

    Add files via upload

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=anorumnr/udacity_kube
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run --image=$dockerpath udacitykube
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udacitykube 8000:80
